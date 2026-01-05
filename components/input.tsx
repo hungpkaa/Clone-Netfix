7cd0e4daf44f888e729644eda86f5e738c5546a5
DsChien102 <22010008@st.phenikaa-uni.edu.vn>
2026-01-05 18:17:05 +0700

    Update auth page and input component

--- components/input.tsx
+++ components/input.tsx
@@ -1,8 +1,19 @@
-const Input = () => {
+import React from 'react';
+interface InputProps {
+  id: string;
+  onChange: any;
+  value: string;
+  label: string;
+  type: string;
+}
+const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
   return (
     <div className="relative">
       <input
-        id="email"
+        type={type}
+        onChange={onChange}
+        value={value}
+        id={id}
         className="
   block
   rounded-md
@@ -36,9 +47,9 @@ const Input = () => {
         peer-placeholder-shown:translate-y-0
         peer-focus:scale-75
         peer-focus:-translate-y-3"
-        htmlFor="email"
+        htmlFor={id}
       >
-        Email
+        {label}
       </label>
     </div>
   );
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
 import Input from '@/components/input';
-export default function Auth() {
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <div className="relative min-h-screen w-full bg-[url('/images/hero.jpg')] bg-cover bg-center bg-no-repeat bg-fixed">
       <div className="absolute inset-0 bg-black/50">
@@ -11,10 +15,39 @@ export default function Auth() {
           <div className="mt-2 w-full rounded-md bg-black/70 px-16 py-16 self-center lg:w-2/5 lg:max-w-md">
             <h2 className="mb-8 text-4xl font-semibold text-white">Sign In</h2>
             <div className="flex flex-col gap-4"></div>
-            <Input />
+            <Input
+              label="Username"
+              onChange={(any) => setName(any.target.value)}
+              id="name"
+              value={name}
+            />
+            <Input
+              label="Email"
+              onChange={(any) => setEmail(any.target.value)}
+              id="email"
+              type="email"
+              value={email}
+            />
+            <Input
+              label="Password"
+              onChange={(any) => setPassword(any.target.value)}
+              id="password"
+              type="password"
+              value={password}
+            />
           </div>
+          <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10  hover:bg-red-700 transition">
+            Login
+          </button>
+          <p className="mt-12 text-neutral-500">
+            FIRST TIME USING NETFLIX?
+            <span className="text-white ml-1 hover:underline cursor-pointer">
+              Creat and account
+            </span>
+          </p>
         </div>
       </div>
     </div>
   );
-}
+};
+export default Auth;
